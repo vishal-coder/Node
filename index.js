617c2c83ef70641d98bd826ce5caa85dc5b91e58
Vishal G <55740408+vishal-coder@users.noreply.github.com>
2022-07-18 16:00:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors());
 // const MONGO_URL = "mongodb://localhost";
 // const MONGO_URL = "mongodb://127.0.0.1"; //  nodejs - 16+
 const MONGO_URL = process.env.MONGO_URL;
-//const MONGO_URL = "mongodb+srv://sa:Ewg7pvR6NkbgPjM@cluster0.wirzb.mongodb.net";
+
 
 async function createConnection() {
   const client = new MongoClient(MONGO_URL);
